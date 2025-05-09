6d556ba428a3718a7ae0c2f3678143a42a30fe93
Qwerty <qwerty@mail.ru>
2025-05-09 18:24:40 +0300

    HPBar fix

--- Assets/Scripts/Objects/TriggerMouseObject.cs
+++ Assets/Scripts/Objects/TriggerMouseObject.cs
@@ -1,31 +1,35 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 [DefaultExecutionOrder(100)]
 public class TriggerMouseObject : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
 
-     GameObject canvas;
-
-    void Awake()
-    {
-        canvas = GameObject.FindWithTag("HPObject");
-        
-    }
-
+    GameObject canvas;
+    [SerializeField] private Sprite _icon;
+    private UnityEngine.UI.Image _currentIcon;
     void Start()
     {
-        canvas.SetActive(false);
+        canvas = GameObject.FindWithTag("HPObject");
+        //canvas.SetActive(false);
+        canvas.transform.position = new Vector3(-306,1048);
+        _icon = GetComponent<Sprite>();
+
     }
 
     public void OnPointerEnter(PointerEventData _data)
     {
         GetComponent<MeshRenderer>().material.color = Color.yellow;
-        canvas.SetActive(true);
+        //canvas.SetActive(true);
+        _currentIcon.sprite = _icon;
+        canvas.transform.position = new Vector3(117, 1048); 
     }
     public void OnPointerExit(PointerEventData _data)
     {
         GetComponent<MeshRenderer>().material.color = Color.gray;
-        canvas.SetActive(false);
+        //canvas.SetActive(false);
+        canvas.transform.position = new Vector3(-306, 1048);
     }
 }
